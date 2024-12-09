a7749da6bbcbcfb40c5cf42242253f6856d78763
Uenderson-Mendes <uemdersonbebe@gmail.com>
2024-12-09 19:46:24 -0300

    atualização de formatacao de codigo

--- src/components/ProcessManager.jsx
+++ src/components/ProcessManager.jsx
@@ -54,7 +54,7 @@ export const ProcessManager = () => {
   }, [processes]);
 
   const handleCreateProcess = () => {
-  
+
     const newProcess = {
       id: processes.length + 1,
       state: 'pronto',
@@ -62,7 +62,7 @@ export const ProcessManager = () => {
       time: 0,
       isRunning: true,
       originalIntervalTime: intervalTime,
-      
+
     };
 
     setProcesses((prevProcesses) => [...prevProcesses, newProcess]);
@@ -83,40 +83,40 @@ export const ProcessManager = () => {
   };
   const handleSaveToPDF = () => {
     const confirmSave = window.confirm('Tem certeza que deseja salvar o relatório em PDF?');
-  
+
     if (confirmSave) {
       const doc = new jsPDF();
-  
+
       // Título
       doc.setFontSize(18);
-      
+
       doc.text('Relatório de Processos', 13, 20);
       const lineYPosition = 45; // Posição Y da linha
       doc.line(14, lineYPosition, 200, lineYPosition);
       // Adicionando o tempo total decorrido no cabeçalho
       const timerActive = document.querySelector('.time')?.innerText.split(': ')[1] || '00:00';
       doc.text(`Tempo Ativo: ${timerActive}`, 14, 40);
-  
+
 
       doc.text(`Total de Processos: ${totalProcessesCount}`, 12, 30);
       doc.line(14, lineYPosition, 200, lineYPosition);
       // Adicionando tabela com os processos
       const tableColumn = ["ID", "Tipo", "Estado", "Tempo Total (s)", "Fatia de Tempo (ms)", "Threads",
-      
+
       ];
       const tableRows = [];
-  
+
       processes.forEach((process) => {
         // Verificar e corrigir a memória alocada
 
-        const memoryAllocated = MemoryManager.getMemoryForProcess 
-        ? MemoryManager.getMemoryForProcess(process.id) // Método para pegar memória alocada de um processo
-        : "0 B"; // Valor padrão se o método não existir ou falhar
-        
+        const memoryAllocated = MemoryManager.getMemoryForProcess
+          ? MemoryManager.getMemoryForProcess(process.id) // Método para pegar memória alocada de um processo
+          : "0 B"; // Valor padrão se o método não existir ou falhar
+
 
         // Verificar e corrigir o número de threads
         const numThreads = threadCounts[process.id] || 0; // Forçar valor default (0) se ausente
-        
+
         // Construir a linha de dados do processo
         const processData = [
           process.id,
@@ -127,41 +127,41 @@ export const ProcessManager = () => {
           numThreads,  // Threads agora sempre terá um valor
           //memoryAllocated , // Garante que a memória sempre tenha valor e unidade
         ];
-      
+
         tableRows.push(processData);
       });
-  
+
       // Adicionando a tabela
       doc.autoTable({
         head: [tableColumn],
         body: tableRows,
         startY: 50 // Ajuste para garantir que a tabela não sobreponha os textos anteriores
       });
-  
+
       // Salvar o PDF
       doc.save('relatorio_processos.pdf');
     }
   };
-  
-  
-  
-  
-  
+
+
+
+
+
   const handleStopAllProcesses = () => {
     const confirmStop = window.confirm('Tem certeza que deseja encerrar todos os processos?');
-    
+
     if (confirmStop) {
       stopAllProcesses(setProcesses, setMemoryAllocations, setIntervals, intervals);
-  
+
       // Reseta contagens ao parar todos os processos
       setTotalProcessesCount(0);
       setTotalMemoryAllocated(0);
-  
+
       // Mensagem de sucesso opcional após encerrar os processos
       window.alert('Todos os processos foram encerrados com sucesso.');
     }
   };
-  
+
 
   const handleAdjustInterval = (adjustment) => {
     setIntervalTime((prevTime) => Math.max(100, prevTime + adjustment)); // Mantém no mínimo 100ms
@@ -188,178 +188,178 @@ export const ProcessManager = () => {
 
   return (
     <div className="container">
-       <div className='corpotime'>
-<div className="d-flex justify-content-between align-items-left bg-white text-dark p-2 mb-3" style={{ width: '100%' }}>
- 
-  <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
-  <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
-    <div className='time'>Tempo ativo: <Timer /></div>
-  </div>
-  </div>
+      <div className='corpotime'>
+        <div className="d-flex justify-content-between align-items-left bg-white text-dark p-2 mb-3" style={{ width: '100%' }}>
+
+          <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
+            <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
+              <div className='time'>Tempo ativo: <Timer /></div>
+            </div>
+          </div>
 
 
-  <div className="me-3 dedlocktst" style={{  padding: '10px' }}>
-  <DeadlockManager processes={processes} />
-  <div className='totalpm'>
-  <h5>Total de Processos: {totalProcesses}</h5>
+          <div className="me-3 dedlocktst" style={{ padding: '10px' }}>
+            <DeadlockManager processes={processes} />
+            <div className='totalpm'>
+              <h5>Total de Processos: {totalProcesses}</h5>
 
 
-  </div>
-  </div>
-  <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
-  </div>
-  </div>
-</div>
-<div className="col-md-3 shadow position-absolute" style={{ right: 0, top: 90, height: '48vh', overflowY: 'auto', backgroundColor: '#bababa;', borderRight: '1px solid #dee2e6' }}>
-      <div className="shadow-sm rounded p-2" style={{ backgroundColor: '#f8f9fa' }}>
-        <h4 className="text-center">Memória</h4>
-        <MemoryManager processes={processes} />
-        
+            </div>
+          </div>
+          <div className="me-3" style={{ width: 'auto', padding: '10px' }}>
+          </div>
+        </div>
       </div>
- 
-    </div>
- 
+      <div className="col-md-3 shadow position-absolute" style={{ right: 0, top: 90, height: '48vh', overflowY: 'auto', backgroundColor: '#bababa;', borderRight: '1px solid #dee2e6' }}>
+        <div className="shadow-sm rounded p-2" style={{ backgroundColor: '#f8f9fa' }}>
+          <h4 className="text-center">Memória</h4>
+          <MemoryManager processes={processes} />
+
+        </div>
+
+      </div>
+
 
 
 
       <h2 className='titulogerenciadorprocesos'>Gerenciador de Processos</h2>
 
-  <div className="form-group col-md-3 corpo">
-  <label htmlFor="processType">Escolha o tipo de processo:</label>
-  <select
-  id="processType"
-  value={processType}
-  onChange={(e) => setProcessType(e.target.value)}
-  className="form-control mb-2 select-highlight"
->
-  <option value="cpu-bound">CPU-bound</option>
-  <option value="i/o-bound">I/O-bound</option>
-  <option value="i/o-bound(fita)">I/O-bound (Fita)</option>
-  <option value="i/o-bound(terminal)">I/O-bound (Terminal)</option>
-  <option value="cpu e i/o-bound(disco)">CPU e I/O-bound (Disco)</option>
-  <option value="cpu e i/o-bound(fita)">CPU e I/O-bound (Fita)</option>
-</select>
+      <div className="form-group col-md-3 corpo">
+        <label htmlFor="processType">Escolha o tipo de processo:</label>
+        <select
+          id="processType"
+          value={processType}
+          onChange={(e) => setProcessType(e.target.value)}
+          className="form-control mb-2 select-highlight"
+        >
+          <option value="cpu-bound">CPU-bound</option>
+          <option value="i/o-bound">I/O-bound</option>
+          <option value="i/o-bound(fita)">I/O-bound (Fita)</option>
+          <option value="i/o-bound(terminal)">I/O-bound (Terminal)</option>
+          <option value="cpu e i/o-bound(disco)">CPU e I/O-bound (Disco)</option>
+          <option value="cpu e i/o-bound(fita)">CPU e I/O-bound (Fita)</option>
+        </select>
 
-<hr></hr>
-  <div className="d-flex justify-content-center align-items-center gap-1 mt-2">
-    <button
-      className="btn btn-secondary"
-      style={{ fontSize: '0.8em', padding: '0.25rem 0.5rem' }}
-      onClick={() => handleAdjustInterval(-100)}
-    >
-      <FontAwesomeIcon icon={faMinus} style={{ fontSize: '0.85em' }} />
-    </button>
-    <p className="text-center mb-0 fatia">Fatia de Tempo: {intervalTime}ms</p>
-    <button
-      className="btn btn-secondary"
-      style={{ fontSize: '0.8em', padding: '0.25rem 0.5rem' }}
-      onClick={() => handleAdjustInterval(100)}
-    >
-      <FontAwesomeIcon icon={faPlus} style={{ fontSize: '0.85em' }} />
-    </button>
-  </div>
-
-
-  <div className="d-flex justify-content-center mt-3">
-  <button className="btn btn-primary" onClick={handleCreateProcess}>
-    Criar
-  </button>
-</div>
+        <hr></hr>
+        <div className="d-flex justify-content-center align-items-center gap-1 mt-2">
+          <button
+            className="btn btn-secondary"
+            style={{ fontSize: '0.8em', padding: '0.25rem 0.5rem' }}
+            onClick={() => handleAdjustInterval(-100)}
+          >
+            <FontAwesomeIcon icon={faMinus} style={{ fontSize: '0.85em' }} />
+          </button>
+          <p className="text-center mb-0 fatia">Fatia de Tempo: {intervalTime}ms</p>
+          <button
+            className="btn btn-secondary"
+            style={{ fontSize: '0.8em', padding: '0.25rem 0.5rem' }}
+            onClick={() => handleAdjustInterval(100)}
+          >
+            <FontAwesomeIcon icon={faPlus} style={{ fontSize: '0.85em' }} />
+          </button>
+        </div>
 
 
- 
-     </div><br></br>
-     <div className="d-flex justify-content-between gap-1 butt" style={{ width: "300px" ,marginLeft:"10px"}}>
-    <button className="btn btn-danger" onClick={handleStopAllProcesses}>Parar</button>
-    <button className="btn btn-success" onClick={handleSaveToPDF}>Salvar</button>
-</div>
+        <div className="d-flex justify-content-center mt-3">
+          <button className="btn btn-primary" onClick={handleCreateProcess}>
+            Criar
+          </button>
+        </div>
 
 
-<br></br>
 
-      
+      </div><br></br>
+      <div className="d-flex justify-content-between gap-1 butt" style={{ width: "300px", marginLeft: "10px" }}>
+        <button className="btn btn-danger" onClick={handleStopAllProcesses}>Parar</button>
+        <button className="btn btn-success" onClick={handleSaveToPDF}>Salvar</button>
+      </div>
 
 
-  
-    <h3 className="text-center">Processos</h3>
-    <hr style={{ margin: '20px 0', borderTop: '2px solid #000000' }} />
+      <br></br>
+
+
+
+
+
+      <h3 className="text-center">Processos</h3>
+      <hr style={{ margin: '20px 0', borderTop: '2px solid #000000' }} />
 
       <div className="process-list mt-3">
-  <div className="process-grid">
-    {processes.map((process) => {
-      const numThreads = threadCounts[process.id] || 0; // Recupera o número de threads fixo para o processo atual
+        <div className="process-grid">
+          {processes.map((process) => {
+            const numThreads = threadCounts[process.id] || 0; // Recupera o número de threads fixo para o processo atual
 
-      // Função para parar o processo específico
-      const handleStopProcess = (processId) => {
-        // Remover o processo da lista de processos
-        setProcesses((prevProcesses) => prevProcesses.filter((p) => p.id !== processId));
-        
-        // Parar o intervalo associado a esse processo
-        clearInterval(intervals[processId]);
-        setIntervals((prevIntervals) => {
-          const { [processId]: _, ...remainingIntervals } = prevIntervals;
-          return remainingIntervals;
-        });
+            // Função para parar o processo específico
+            const handleStopProcess = (processId) => {
+              // Remover o processo da lista de processos
+              setProcesses((prevProcesses) => prevProcesses.filter((p) => p.id !== processId));
 
-       // Remove a alocação de memória associada e atualiza a memória total
-    setMemoryAllocations((prevAllocations) => {
-      const memory = prevAllocations[processId];
-      const { [processId]: _, ...remainingAllocations } = prevAllocations;
-      setTotalMemoryAllocated((prevTotal) => prevTotal - memory); // Subtrai a memória do total
-      return remainingAllocations;
-    });
+              // Parar o intervalo associado a esse processo
+              clearInterval(intervals[processId]);
+              setIntervals((prevIntervals) => {
+                const { [processId]: _, ...remainingIntervals } = prevIntervals;
+                return remainingIntervals;
+              });
 
-        // Remover o contador de threads associado
-        setThreadCounts((prevCounts) => {
-          const { [processId]: _, ...remainingCounts } = prevCounts;
-          return remainingCounts;
-        });
-        
-        // Atualizar o estado do semáforo
-        setSemaphoreStates((prevStates) => {
-          const { [processId]: _, ...remainingStates } = prevStates;
-          return remainingStates;
-        });
-      };
-      
-      return (
-          
-        <div key={process.id} className="process-card ">
-          
-        <div className="titulo-status">
-            <span className={`status-${process.state.replace(' ', '-')}`}>{process.state}</span>
-            <p className="titulo">Processo #{process.id} ({process.type})</p>
+              // Remove a alocação de memória associada e atualiza a memória total
+              setMemoryAllocations((prevAllocations) => {
+                const memory = prevAllocations[processId];
+                const { [processId]: _, ...remainingAllocations } = prevAllocations;
+                setTotalMemoryAllocated((prevTotal) => prevTotal - memory); // Subtrai a memória do total
+                return remainingAllocations;
+              });
+
+              // Remover o contador de threads associado
+              setThreadCounts((prevCounts) => {
+                const { [processId]: _, ...remainingCounts } = prevCounts;
+                return remainingCounts;
+              });
+
+              // Atualizar o estado do semáforo
+              setSemaphoreStates((prevStates) => {
+                const { [processId]: _, ...remainingStates } = prevStates;
+                return remainingStates;
+              });
+            };
+
+            return (
+
+              <div key={process.id} className="process-card ">
+
+                <div className="titulo-status">
+                  <span className={`status-${process.state.replace(' ', '-')}`}>{process.state}</span>
+                  <p className="titulo">Processo #{process.id} ({process.type})</p>
+                </div>
+                <p>Tempo Total: {process.time}s</p>
+                <p>Fatia de tempo original: {process.originalIntervalTime}ms</p>
+
+                <p className='threads'>
+                  {process.state === 'pronto'
+                    ? `Processo está pronto com ${numThreads} threads prontas, aguardando execução.`
+                    : process.state === 'em espera'
+                      ? `Processo está esperando (bloqueado) com ${numThreads} threads prontas, sem execução.`
+                      : process.state === 'em execução'
+                        ? `Processo está em execução com ${numThreads} threads em execução.`
+                        : `Processo está ${process.state} com ${numThreads} threads.`}
+                </p>
+
+                {/* Footer do card com o botão de fechar */}
+                <div className="card-footer">
+                  <button className="btn btn-danger btn-sm" onClick={() => handleStopProcess(process.id)}>
+                    Fechar
+                  </button>
+                </div>
+              </div>
+
+            );
+          })}
         </div>
-        <p>Tempo Total: {process.time}s</p>
-        <p>Fatia de tempo original: {process.originalIntervalTime}ms</p>
-    
-        <p className='threads'>
-            {process.state === 'pronto'
-                ? `Processo está pronto com ${numThreads} threads prontas, aguardando execução.`
-                : process.state === 'em espera'
-                ? `Processo está esperando (bloqueado) com ${numThreads} threads prontas, sem execução.`
-                : process.state === 'em execução'
-                ? `Processo está em execução com ${numThreads} threads em execução.`
-                : `Processo está ${process.state} com ${numThreads} threads.`}
-        </p>
-    
-        {/* Footer do card com o botão de fechar */}
-        <div className="card-footer">
-            <button className="btn btn-danger btn-sm" onClick={() => handleStopProcess(process.id)}>
-                Fechar
-            </button>
-        </div>
-    </div>
-    
-      );
-    })}
-  </div>
-</div>
-<br></br>
+      </div>
+      <br></br>
 
 
       <Semaphore processes={processes} semaphoreStates={semaphoreStates} />
-      
+
     </div>
   );
 };
